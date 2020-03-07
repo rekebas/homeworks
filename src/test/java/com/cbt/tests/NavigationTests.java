f6a324efd16bd5da042adc8783dc26f367b65248
rekebas <aekebas@hotmail.com>
2020-03-07 02:09:42 -0600

    update

---
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -0,0 +1,75 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class NavigationTests {
+
+
+    WebDriver driver;
+
+
+
+public static void ChromeTest() {
+
+      WebDriver driver = BrowserFactory.getDriver("Chrome");
+
+
+    driver.manage().window().maximize();
+   driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    driver.navigate().to("http://www.google.com");
+    String titleGoogle = driver.getTitle();
+    driver.get("http//www.etsy.com");
+    String titleEtsy = driver.getTitle();
+    driver.navigate().back();
+    System.out.println(driver.getTitle().equalsIgnoreCase(titleGoogle));
+    driver.navigate().forward();
+    System.out.println(driver.getTitle().equalsIgnoreCase(titleEtsy));
+}
+
+    public static void firefoxTest() {
+
+        WebDriver driver = BrowserFactory.getDriver("firefox");
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.navigate().to("http://www.google.com");
+        String titleGoogle = driver.getTitle();
+        driver.get("http//www.etsy.com");
+        String titleEtsy = driver.getTitle();
+        driver.navigate().back();
+        System.out.println(driver.getTitle().equalsIgnoreCase(titleGoogle));
+        driver.navigate().forward();
+        System.out.println(driver.getTitle().equalsIgnoreCase(titleEtsy));
+    }
+
+    public static void edgeTest() {
+
+        WebDriver driver = BrowserFactory.getDriver("edge");
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.navigate().to("http://www.google.com");
+        String titleGoogle = driver.getTitle();
+        driver.get("http//www.etsy.com");
+        String titleEtsy = driver.getTitle();
+        driver.navigate().back();
+        System.out.println(driver.getTitle().equalsIgnoreCase(titleGoogle));
+        driver.navigate().forward();
+        System.out.println(driver.getTitle().equalsIgnoreCase(titleEtsy));
+    }
+
+
+}
+
+
+
--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,5 +1,32 @@
 package com.cbt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
 public class BrowserFactory {
 
-}
+
+
+        public static WebDriver getDriver (String browserName){
+            if (browserName.equalsIgnoreCase("chrome")){
+                WebDriverManager.chromedriver().version("79.0").setup();
+                return new ChromeDriver();
+            }else if (browserName.equalsIgnoreCase("firefox")){
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            }else if (browserName.equalsIgnoreCase("opera")){
+                WebDriverManager.operadriver().setup();
+                return new OperaDriver();
+            }else if (browserName.equalsIgnoreCase("edge")){
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+            }
+            return null;
+        }
+
+    }
+
