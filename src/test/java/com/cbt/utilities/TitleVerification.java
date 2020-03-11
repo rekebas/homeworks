72c14d50cab57001ddff4aa40e937d8db6a49da8
rekebas <aekebas@hotmail.com>
2020-03-10 23:44:52 -0500

    update

---
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -0,0 +1,61 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class TitleVerification {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<String> urls = Arrays.asList("http://practice.cybertekschool.com/",
+                "http://practice.cybertekschool.com/dropdown", "http://practice.cybertekschool.com/login");
+
+        WebDriver driver = BrowserFactory.getDriver("Chrome");
+
+
+        List<String> texts = new ArrayList<>();
+                for (int i = 0; i <urls.size() ; i++) {
+
+                    driver.get(urls.get(i));
+
+                    texts.add(driver.getTitle());
+                    Thread.sleep(3000);
+
+
+                }
+          String expectedTitle = texts.get(0);
+                String expectedURL ="http://practice.cybertekschool.com/";
+                if (texts.get(0).equalsIgnoreCase(expectedTitle)&&
+                        (texts.get(1).equalsIgnoreCase(expectedTitle))&&texts.get(2).equalsIgnoreCase(expectedTitle)){
+                    System.out.println(" The Webpages Have The Same Title. TEST PASSED! ");
+                }
+                else {
+
+
+                     System.out.println(" The Webpages Don't Have The Same Title. TEST FAILED!!! ");
+                }
+
+                if (urls.get(0).startsWith("http://practice.cybertekschool.com/")&&
+                (urls.get(1).startsWith("http://practice.cybertekschool.com/"))&&urls.get(2).startsWith("http://practice.cybertekschool.com/")){
+            System.out.println(" The Webpages Have The Correct URL. TEST PASSED! ");
+        }
+                else {
+
+
+            System.out.println(" The Webpages Don't Have Correct URL. TEST FAILED!!! ");
+        }
+
+
+
+        driver.quit();
+
+    }
+}
+
+
+
